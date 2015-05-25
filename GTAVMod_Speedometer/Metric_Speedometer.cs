70bd0fb099677e26219ffbcde17d9e05be571baf
Wenchao Wang <unlock.liberty@gmail.com>
2015-05-25 17:24:34 -0400

    updated to 2.1.0

--- GTAVMod_Speedometer/Metric_Speedometer.cs
+++ GTAVMod_Speedometer/Metric_Speedometer.cs
@@ -1,7 +1,7 @@
 ﻿/*
  * Simple Metric/Imperial Speedometer
  * Author: libertylocked
- * Version: 2.0.4b
+ * Version: 2.1.0
  * License: GPLv2
  */
 using System;
@@ -23,7 +23,7 @@ namespace GTAVMod_Speedometer
     public class Metric_Speedometer : Script
     {
         // Constants
-        public const string SCRIPT_VERSION = "2.0.4b";
+        public const string SCRIPT_VERSION = "2.1.0";
         const string URL_VERSIONFILE = @"https://raw.githubusercontent.com/LibertyLocked/GTAVMod_Speedometer/release/GTAVMod_Speedometer/version.txt"; // latest ver text
         const int NUM_FONTS = 8;
         const float RAINBOW_FRAMETIME = 0.034f;
